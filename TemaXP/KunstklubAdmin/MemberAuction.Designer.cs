f7db9b2869e696a54ad3f043e8924c46179d8a0a
Hamun <commentatorbot@gmail.com>
2015-09-04 13:27:46 +0200

    Lavet Lidt af gui til MemberAuction

--- TemaXP/KunstklubAdmin/MemberAuction.Designer.cs
+++ TemaXP/KunstklubAdmin/MemberAuction.Designer.cs
@@ -28,47 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.medlemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.kunstværkerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.omToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MemberName = new System.Windows.Forms.Label();
             this.MemberEmail = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.menuStrip1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.InfoPainting = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.medlemToolStripMenuItem,
-            this.kunstværkerToolStripMenuItem,
-            this.omToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(862, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // medlemToolStripMenuItem
-            // 
-            this.medlemToolStripMenuItem.Name = "medlemToolStripMenuItem";
-            this.medlemToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
-            this.medlemToolStripMenuItem.Text = "Medlem";
-            // 
-            // kunstværkerToolStripMenuItem
-            // 
-            this.kunstværkerToolStripMenuItem.Name = "kunstværkerToolStripMenuItem";
-            this.kunstværkerToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
-            this.kunstværkerToolStripMenuItem.Text = "Kunstværker";
-            // 
-            // omToolStripMenuItem
-            // 
-            this.omToolStripMenuItem.Name = "omToolStripMenuItem";
-            this.omToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
-            this.omToolStripMenuItem.Text = "Hjælp";
             // 
             // MemberName
             // 
@@ -96,36 +62,50 @@
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(146, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 131);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InfoPainting
+            // 
+            this.InfoPainting.AutoSize = true;
+            this.InfoPainting.Location = new System.Drawing.Point(143, 196);
+            this.InfoPainting.Name = "InfoPainting";
+            this.InfoPainting.Size = new System.Drawing.Size(131, 13);
+            this.InfoPainting.TabIndex = 5;
+            this.InfoPainting.Text = "Information om Kunstværk";
+            // 
             // MemberAuction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(862, 457);
+            this.Controls.Add(this.InfoPainting);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.MemberEmail);
             this.Controls.Add(this.MemberName);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MemberAuction";
-            this.Text = "Start";
+            this.Text = "UCN Kunst klub";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.MemberAuction_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem medlemToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem kunstværkerToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem omToolStripMenuItem;
         private System.Windows.Forms.Label MemberName;
         private System.Windows.Forms.Label MemberEmail;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label InfoPainting;
     }
 }
 
