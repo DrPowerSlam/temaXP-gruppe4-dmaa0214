1ffe26fef4dc3f4bfbf53b88f4c123f5b698a671
LK28 <lau.kondrup@gmail.com>
2015-09-04 12:47:38 +0200

    CreateFind Auction

---
+++ TemaXP/TemaXP/DBLayer/AuctionRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemaXP.ModelLayer;
+
+namespace TemaXP.DBLayer
+{
+    public class AuctionRepository : SuperRepository
+    {
+
+        public void InsertAuction(Auction auction)
+        {
+            context.Auctions.Add(auction);
+            Save();
+        }
+
+        public Auction FindAuction(int id)
+        {
+            return context.Auctions.Find(id);
+        }
+
+        public void DeleteAuction(int id)
+        {
+            context.Auctions.Remove(FindAuction(id));
+            Save();
+        }
+
+        public List<Auction> GetAllAuctions()
+        {
+            return context.Auctions.ToList();
+        }
+    }
+}
---
+++ TemaXP/Tests/AuctionTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemaXP.CtrLayer;
+using TemaXP.ModelLayer;
+
+namespace Tests
+{
+    [TestClass]
+    public class AuctionTest
+    {
+        private AuctionCtr ctr;
+        private Auction auction;
+
+        [TestInitialize]
+        public void AuctionInitialize()
+        {
+            ctr = new AuctionCtr();
+
+            List<Member> members = new List<Member>()
+            {
+                new Member(),
+                new Member(),
+
+            };
+
+            List<Painting> paintings = new List<Painting>()
+            {
+                new Painting(),
+                new Painting(),
+            };  
+            
+            auction = new Auction()
+            {
+                Date = DateTime.Now,
+                Members = members,
+                Paintings = paintings
+            };
+        }
+
+        [TestCleanup]
+        public void AuctionCleanUp()
+        {
+            ctr = null;
+            auction = null;
+        }
+
+        [TestMethod]
+        public void CreateFindAuction()
+        {
+            ctr.InsertAuction(auction);
+            Auction foundAuction = ctr.FindAuction(auction.ID);
+            Assert.AreEqual(auction.ID, foundAuction.ID);
+            ctr.DeleteAuction(foundAuction.ID);
+        }
+    }
+}
